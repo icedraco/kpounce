7d2bedc92fdf7174e59705f8e5839b27347a6362
IceDragon <icedragon@quickfox.org>
2017-06-02 19:07:56 +0300

    added continuous polling (instead of a one-off)

--- src/PouncePoller.kt
+++ src/PouncePoller.kt
@@ -1,6 +1,10 @@
 import pounce.Pounce
+import java.text.SimpleDateFormat
+import java.util.*
 
 const val NAME_PADDING = 16
+
+fun getTimestamp(): String = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US).format(Date())
 
 fun main(args: Array<String>) {
     val client = Pounce()
@@ -26,5 +30,14 @@ fun main(args: Array<String>) {
     println("Dreams:")
     for (rec in client.dreams) {
         println("  * ${rec.subject.toString().padEnd(NAME_PADDING)}  [${rec.status}]")
+    }
+
+    // continuous polling
+    while (true) {
+        client.update(
+                { rec, status -> println("${getTimestamp()} >> ${rec.subject}: ${rec.status} -> $status") },
+                { rec, status -> println("${getTimestamp()} >> ${rec.subject}: ${rec.status} -> $status") })
+
+        Thread.sleep(client.requestIntervalMillis.toLong())
     }
 }
